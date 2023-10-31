d8f3c1d25d92549098fb131e51e5f17d7e9546a5
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-11-01 01:45:15 +0300

    comm

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -21,12 +21,22 @@ class SearchBar extends Component {
     ) {
       return Notify.failure('This is current request');
     }
+
+    this.setState({
+      lastRequest: this.state.value.toLowerCase().trim(),
+    });
     this.props.onSubmit(this.state.value.toLowerCase().trim());
     this.setState({
       value: '',
-      lastRequest: this.state.value.toLowerCase().trim(),
     });
   };
+
+  //   this.props.onSubmit(this.state.value.toLowerCase().trim());
+  //   this.setState({
+  //     value: '',
+  //     lastRequest: this.state.value.toLowerCase().trim(),
+  //   });
+  // };
   render() {
     return (
       <header className="searchbar">
@@ -54,3 +64,8 @@ export default SearchBar;
 SearchBar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+
+
+
+
